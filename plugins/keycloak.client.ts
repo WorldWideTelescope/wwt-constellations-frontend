590de8dfa79f410d34909467150949925750e16c
Jon Carifio <carifio24@gmail.com>
2023-02-06 08:58:07 -0500

    Don't init keycloak on startup for now.

--- plugins/keycloak.client.ts
+++ plugins/keycloak.client.ts
@@ -13,28 +13,34 @@ export default defineNuxtPlugin(nuxtApp => {
   };
 
   const keycloak = new Keycloak(config);
-  keycloak.init(initOptions).then((auth) => {
-    if (!auth) {
-      window.location.reload();
-    } else {
-      console.log("Authenticated!");
-      nuxtApp.vueApp.provide("keycloak", keycloak);
-    }
+  //keycloak.init(initOptions); // causes a state refresh
+  nuxtApp.provide("keycloak", keycloak);
 
-    // Token refresh
-    setInterval(() => {
-      keycloak.updateToken(70).then((refreshed) => {
-        if (refreshed) {
-          console.log("Token refreshed " + refreshed);
-        } else {
-          console.warn(`Token not refreshed, valid for ${Math.round(keycloak.tokenParsed!.exp! + keycloak.timeSkew! - new Date().getTime() / 1000)} seconds`);
-        }
-      }).catch(() => {
-        console.error("Failed to refresh token");
-      })
-    }, 6000);
+//   keycloak.init(initOptions).then((auth) => {
+//     if (!auth) {
+//       window.location.reload();
+//     } else {
+//       console.log("Authenticated!");
+//       nuxtApp.vueApp.provide("keycloak", keycloak);
+//     }
 
-  }).catch(() => {
-    console.error("Authentication failed");
-  });
+//     // Token refresh
+//     setInterval(() => {
+//       keycloak.updateToken(70).then((refreshed) => {
+//         if (refreshed) {
+//           console.log("Token refreshed " + refreshed);
+//         } else {
+//           console.warn(`Token not refreshed, valid for ${Math.round(keycloak.tokenParsed!.exp! + keycloak.timeSkew! - new Date().getTime() / 1000)} seconds`);
+//         }
+//       }).catch(() => {
+//         console.error("Failed to refresh token");
+//       })
+//     }, 6000);
+
+//   }).catch(() => {
+//     console.error("Authentication failed");
+//   });
+
+
 });
+
