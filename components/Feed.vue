f941b081b686790719e036b5ab073a5ea31f052b
Peter Williams <peter@newton.cx>
2023-03-20 12:39:36 -0400

    components/Feed.vue: avoid mixed-content warnings

--- components/Feed.vue
+++ components/Feed.vue
@@ -97,7 +97,7 @@ export default defineNuxtComponent({
     log(x: any) { console.log(x); },
     async loadItems(page: number): Promise<Item[]> {
       //const url = `http://${nuxtConfig.apiUrl}/data?page=${page}&limit=${this.pageSize}`;
-      const url = "http://data1.wwtassets.org/packages/2022/07_jwst/jwst_first_v2.wtml";
+      const url = "https://data1.wwtassets.org/packages/2022/07_jwst/jwst_first_v2.wtml";
       const store = this.$engineStore(this.$pinia);
       const folder = await store.loadImageCollection({
         url: url,
