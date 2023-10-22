ee9b1bc5a84b31cffbfdd1687da4e953a0b432a9
Peter Williams <peter@newton.cx>
2023-10-22 16:25:13 -0400

    plugins/vue-gtag.client.ts: make the Google Analytics request dynamic
    If the user clicks "Accept" on the cookie banner, we'll now send a
    request immediately, instead of merely saving the setting and not doing
    anything about it until the next HTTP request comes in. This will make a
    difference for people who only ever visit once, if they accept the
    cookies.

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -2,11 +2,21 @@ import VueGtag from "vue-gtag";
 
 export default defineNuxtPlugin(nuxtApp => {
   const nuxtConfig = useRuntimeConfig();
-  const cookieControl = useCookieControl();
 
-  if (nuxtConfig.public.googleAnalyticsTag && cookieControl.cookiesEnabledIds.value?.includes('ga')) {
-    nuxtApp.vueApp.use(VueGtag, {
-      config: { id: nuxtConfig.public.googleAnalyticsTag }
+  if (nuxtConfig.public.googleAnalyticsTag) {
+    const cookieControl = useCookieControl();
+
+    // By watching the cookies-enabled setting, we can react to the user's
+    // acceptance of the cookie banner and send the analytics request
+    // immediately. This matters because if we're not responsive like that,
+    // we'll never get a report for a visitor who only loads the page once but
+    // *does* agree to accept cookies.
+    watchEffect(() => {
+      if (cookieControl.cookiesEnabledIds.value?.includes('ga')) {
+        nuxtApp.vueApp.use(VueGtag, {
+          config: { id: nuxtConfig.public.googleAnalyticsTag }
+        });
+      }
     });
   }
 });
