f86e5851cdbb643cc747112e9f347b8fc8b581ba
Peter Williams <peter@newton.cx>
2024-02-13 13:00:54 -0500

    Port from fp/io-ts to effect-ts
    As hoped, this seems to fix our build issues. And the port is pretty
    straightforward; nearly doable with search-and-replace. Not yet
    extensively tested but it seems to be working ...

--- utils/types.ts
+++ utils/types.ts
@@ -1,127 +1,111 @@
 // Copyright 2023 the .NET Foundation
 
-import * as t from "io-ts";
+import * as S from "@effect/schema/Schema";
 
 import { Color } from "@wwtelescope/engine";
 
-export const PlaceDetails = t.type({
-  ra_rad: t.number,
-  dec_rad: t.number,
-  roll_rad: t.union([t.number, t.undefined]),
-  roi_height_deg: t.number,
-  roi_aspect_ratio: t.number,
+export const PlaceDetails = S.struct({
+  ra_rad: S.number,
+  dec_rad: S.number,
+  roll_rad: S.union(S.number, S.undefined),
+  roi_height_deg: S.number,
+  roi_aspect_ratio: S.number,
 });
 
-export type PlaceDetailsT = t.TypeOf<typeof PlaceDetails>;
+export type PlaceDetailsT = S.Schema.To<typeof PlaceDetails>;
 
-export const ImageWwt = t.type({
-  base_degrees_per_tile: t.number,
-  bottoms_up: t.boolean,
-  center_x: t.number,
-  center_y: t.number,
-  file_type: t.string,
-  offset_x: t.number,
-  offset_y: t.number,
-  projection: t.string,
-  quad_tree_map: t.string,
-  rotation: t.number,
-  tile_levels: t.number,
-  width_factor: t.number,
-  thumbnail_url: t.string,
+export const ImageWwt = S.struct({
+  base_degrees_per_tile: S.number,
+  bottoms_up: S.boolean,
+  center_x: S.number,
+  center_y: S.number,
+  file_type: S.string,
+  offset_x: S.number,
+  offset_y: S.number,
+  projection: S.string,
+  quad_tree_map: S.string,
+  rotation: S.number,
+  tile_levels: S.number,
+  width_factor: S.number,
+  thumbnail_url: S.string,
 });
 
-export type ImageWwtT = t.TypeOf<typeof ImageWwt>;
+export type ImageWwtT = S.Schema.To<typeof ImageWwt>;
 
-export const ImageStorage = t.type({
-  legacy_url_template: t.union([t.string, t.undefined]),
+export const ImageStorage = S.struct({
+  legacy_url_template: S.union(S.string, S.undefined),
 });
 
-export type ImageStorageT = t.TypeOf<typeof ImageStorage>;
+export type ImageStorageT = S.Schema.To<typeof ImageStorage>;
 
-export const ImagePermissions = t.intersection([
-  t.type({
-    copyright: t.string,
-    license: t.string,
-  }),
-  t.partial({
-    credits: t.string,
-  })
-]);
-
-export type ImagePermissionsT = t.TypeOf<typeof ImagePermissions>;
-
-export const ImageDisplayInfo = t.intersection([
-  t.type({
-    id: t.string,
-    wwt: ImageWwt,
-    storage: ImageStorage,
-    permissions: ImagePermissions,
-  }),
-  t.partial({
-    alt_text: t.string,
-  })
-]);
-
-export type ImageDisplayInfoT = t.TypeOf<typeof ImageDisplayInfo>;
-
-export const SceneImageLayer = t.type({
-  image_id: t.string,
-  opacity: t.number,
+export const ImagePermissions = S.struct({
+  copyright: S.string,
+  license: S.string,
+  credits: S.optional(S.string, { exact: true }),
 });
 
-export type SceneImageLayerT = t.TypeOf<typeof SceneImageLayer>;
+export type ImagePermissionsT = S.Schema.To<typeof ImagePermissions>;
 
-export const SceneImageLayerHydrated = t.type({
+export const ImageDisplayInfo = S.struct({
+  id: S.string,
+  wwt: ImageWwt,
+  storage: ImageStorage,
+  permissions: ImagePermissions,
+  alt_text: S.optional(S.string, { exact: true }),
+});
+
+export type ImageDisplayInfoT = S.Schema.To<typeof ImageDisplayInfo>;
+
+export const SceneImageLayer = S.struct({
+  image_id: S.string,
+  opacity: S.number,
+});
+
+export type SceneImageLayerT = S.Schema.To<typeof SceneImageLayer>;
+
+export const SceneImageLayerHydrated = S.struct({
   image: ImageDisplayInfo,
-  opacity: t.number,
+  opacity: S.number,
 });
 
-export type SceneImageLayerHydratedT = t.TypeOf<typeof SceneImageLayerHydrated>;
+export type SceneImageLayerHydratedT = S.Schema.To<typeof SceneImageLayerHydrated>;
 
-export const SceneContent = t.type({
-  image_layers: t.array(SceneImageLayer),
+export const SceneContent = S.struct({
+  image_layers: S.array(SceneImageLayer),
 });
 
-export type SceneContentT = t.TypeOf<typeof SceneContent>;
+export type SceneContentT = S.Schema.To<typeof SceneContent>;
 
-export const SceneContentHydrated = t.intersection([
-  t.partial({
-    background: ImageDisplayInfo,
-  }),
-  t.type({
-    image_layers: t.union([t.array(SceneImageLayerHydrated), t.undefined]),
-  })
-]);
+export const SceneContentHydrated = S.struct({
+  image_layers: S.union(S.array(SceneImageLayerHydrated), S.undefined),
+  background: S.optional(ImageDisplayInfo, { exact: true })
+});
 
-export type SceneContentHydratedT = t.TypeOf<typeof SceneContentHydrated>;
+export type SceneContentHydratedT = S.Schema.To<typeof SceneContentHydrated>;
 
-export const SceneDisplayInfo = t.type({
-  id: t.string,
+export const SceneDisplayInfo = S.struct({
+  id: S.string,
   place: PlaceDetails,
   content: SceneContentHydrated,
 });
 
-export type SceneDisplayInfoT = t.TypeOf<typeof SceneDisplayInfo>;
+export type SceneDisplayInfoT = S.Schema.To<typeof SceneDisplayInfo>;
 
-export const SceneCreationInfo = t.intersection([
-  t.partial({
-    outgoing_url: t.string,
-  }),
-  t.type({
-    place: PlaceDetails,
-    content: SceneContent,
-    text: t.string,
-  })
-]);
-
-export type SceneCreationInfoT = t.TypeOf<typeof SceneCreationInfo>;
-
-export const ScenePreviews = t.partial({
-  video: t.string,
-  thumbnail: t.string,
+export const SceneCreationInfo = S.struct({
+  place: PlaceDetails,
+  content: SceneContent,
+  text: S.string,
+  outgoing_url: S.optional(S.string, { exact: true }),
 });
 
-export type ScenePreviewsT = t.TypeOf<typeof ScenePreviews>;
+export type SceneCreationInfoT = S.Schema.To<typeof SceneCreationInfo>;
+
+export const ScenePreviews = S.struct({
+  video: S.optional(S.string, { exact: true }),
+  thumbnail: S.optional(S.string, { exact: true }),
+});
+
+export type ScenePreviewsT = S.Schema.To<typeof ScenePreviews>;
 
 export interface SkymapSceneInfo {
   id: string;
@@ -133,16 +117,16 @@ export interface SkymapSceneInfo {
   adjacent: boolean;
 }
 
-export const TessellationCell = t.type({
-  neighbors: t.array(t.string),
-  location: t.type({
-    ra: t.number,
-    dec: t.number,
+export const TessellationCell = S.struct({
+  neighbors: S.array(S.string),
+  location: S.struct({
+    ra: S.number,
+    dec: S.number,
   }),
-  scene_id: t.string
+  scene_id: S.string
 });
 
-export type TessellationCellT = t.TypeOf<typeof TessellationCell>;
+export type TessellationCellT = S.Schema.To<typeof TessellationCell>;
 
 // Older types, potentially to be removed:
 
